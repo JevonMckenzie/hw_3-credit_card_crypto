c4328d71e87b3772af55d157151a4a9f6469f86b
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2015-04-09 21:00:30 +0800

    Added implementation for aes cipher

--- aes_cipher.rb
+++ aes_cipher.rb
@@ -2,14 +2,31 @@ require 'openssl'
 
 module AesCipher
   def self.encrypt(document, key)
-    # TODO: Return JSON string of array: [iv, ciphertext]
-    #       where iv is the random intialization vector used in AES
-    #       and ciphertext is the output of AES encryption
-    # NOTE: Use hexadecimal strings for output so that it is screen printable
-    #       Use cipher block chaining mode only!
+    # unpack to bits
+    key_length = key.to_s.unpack('B*').join.length
+    # number of bits must be 128, 192, or 256
+    unless [128, 192, 256].include? key_length
+      fail ArgumentError, 'key length must be 128, 192 or 256 bits'
+    end
+    aes_cipher = OpenSSL::Cipher::AES.new(key_length, :CBC).encrypt
+    aes_cipher.key = key
+    [
+      aes_cipher.random_iv,
+      aes_cipher.update(document.to_s) + aes_cipher.final
+    ].map { |str| str.unpack('H*').join }.to_json
   end
 
-  def self.encrypt(aes_crypt, key)
-    # TODO: decrypt from JSON output (aes_crypt) of encrypt method above
+  def self.decrypt(aes_crypt, key)
+    # unpack to bits
+    key_length = key.to_s.unpack('B*').join.length
+    # number of bits must be 128, 192, or 256
+    unless [128, 192, 256].include? key_length
+      fail ArgumentError, 'key length must be 128, 192 or 256 bits'
+    end
+    iv_cipher = JSON.parse(aes_crypt).map { |val| [val].pack('H*') }
+    aes_decipher = OpenSSL::Cipher::AES.new(key_length, :CBC).decrypt
+    aes_decipher.iv = iv_cipher.first
+    aes_decipher.key = key
+    aes_decipher.update(iv_cipher.last) + aes_decipher.final
   end
 end
