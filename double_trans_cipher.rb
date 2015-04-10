8bf24d6a9977e2cf1611aa4d3883a27ae003d16d
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2015-04-11 01:34:44 +0800

    Added comments to code

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,20 +1,21 @@
-require 'gsl'
+require 'gsl' #requiring GSL library to use swap rows() and swap cols()
 
 module DoubleTranspositionCipher
-  def self._swap(mat, iterations, rand_key, swap_rows)
-    i = 0
+  def self._swap(mat, iterations, rand_key, swap_rows) #defining method to swap rows and columns specified by a random number
+    i = 0 
+            #initializing counters to swap rows and columns
     j = 2
-    key_int = rand_key.rand(128)
-    (0..key_int).each{
-      if i == iterations-2
-        i = 0
+    key_int = rand_key.rand(128)  #assigning random number to key_int
+    (0..key_int).each{            #entering loop, controlled by random variable/number
+      if i == iterations-2        #check to see if counter has reached the end of the matrix
+        i = 0                     #if counter reached the end, setting counter to continue swapping from begining of matrix
       end
       if swap_rows == true
-        mat = mat.swap_rows(i,j+i)
+        mat = mat.swap_rows(i,j+i)  #using GSL function swap_rows() to swap rows
       else
-        mat = mat.swap_cols(i,j+i)
+        mat = mat.swap_cols(i,j+i)  #using GSL function swap_cols() to swap cols
       end
-      i += 1
+      i += 1          #incrementing counter to allow subsequent columns and rows to swap
     }
     return mat
   end
@@ -27,25 +28,29 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
-    len = document.length
-    rows = (len**0.5).round
-    cols = (len/rows.to_f).round
-    rand_key = Random.new(key)
-    rand_key.rand(128)
+    len = document.length         #assigning length of document to len
+    rows = (len**0.5).round       #finding the square root of the len and assigning the rounded value to rows
+                                  #finding the two closest factors to len(lenght of document) to get dimensions matrix
+    cols = (len/rows.to_f).round  #finding the numbers of clomuns
+    rand_key = Random.new(key)    #passing key as seed to generate random variable
+    rand_key.rand(128)            #generating random variable
 
-    (len..rows*cols).each{|x|document[x] = '+'}
-    content = document.split("").map{|c|c.ord}
-    vec = GSL::Vector::Int.alloc(*content)
-    mat = vec.matrix_view(rows,cols)
-    mat = _swap(mat, rows, rand_key, true)
-    mat = _swap(mat, cols, rand_key, false)
-    encrypted_text = '';
-    mat.to_v.each{|x|
+    (len..rows*cols).each{|x|document[x] = '+'} #assigning the character '+' to padd the matrix 
+    content = document.split("").map{|c|c.ord}  #creating array to store the ascii representation of the characters from the document
+      #Note: did so because GSL matrix only stores floating point representation(cannot represent chars in floating point)
+    vec = GSL::Vector::Int.alloc(*content)      #using GSL vector to convert the the matrix floating point elements to integer
+                                                #and using (*), i.e. *content to pass elements of matrix to vec
+    mat = vec.matrix_view(rows,cols)            #assigning the rows and columns to form matrix named mat
+    mat = _swap(mat, rows, rand_key, true)      #parameter for method self._swap
+    mat = _swap(mat, cols, rand_key, false)     #parameter for method self._swap
+    encrypted_text = '';                        
+    mat.to_v.each{|x|                         #looping through matrix and converting each integer to a character
       encrypted_text << x.chr
     }
     return encrypted_text
   end
-
+  #because using the same seed (key) in random generator generates the same random number,
+  #the decrypt is just the opposite of encrypt using the same seed (key)
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
     len = ciphertext.length
